b3ee8043ad8b562071ac557e0e8ff451d438692d
OnikepeAz <azeezonikepe@gmail.com>
2020-11-05 12:42:30 +0100

    changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Home from "./home";
+import Footer from "./footer";
+
+
+function App() {
+  return (
+    <div >
+      <Home/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
